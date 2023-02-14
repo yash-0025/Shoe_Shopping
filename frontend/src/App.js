70f42489bd9bca95e696be00644fa0c9be2bccf0
Yash Patel <yashpatel200403@gmail.com>
2023-02-14 12:35:07 +0530

    Project Starter Code initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-2xl font-bold underline'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
